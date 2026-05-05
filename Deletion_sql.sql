0544cd1a3221ada78f5f76ca6486ff88b5c6d8e7
Navneet Singh <38272714+navneet-singh2907@users.noreply.github.com>
2026-05-04 22:48:58 -0400

    feat: mastered data removal using DELETE and DROP TABLE

---
+++ Deletion_sql.sql
@@ -0,0 +1,31 @@
+
+DELETE FROM student
+WHERE id = 2;
+
+
+DELETE FROM student
+WHERE id = 2;
+
+DELETE FROM student
+WHERE grade = '9th';
+
+
+DELETE FROM student
+WHERE age < 16;
+
+
+
+DELETE FROM student
+WHERE grade IS NULL;
+
+
+
+DELETE FROM student;
+
+
+
+
+DROP TABLE student;
+
+
+SELECT * FROM student;
