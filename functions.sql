69fed79a954efdf7688c1878bfbfd939fc12c02c
Navneet Singh <38272714+navneet-singh2907@users.noreply.github.com>
2026-05-06 00:35:27 -0400

    feat: implemented aggregate and scalar functions with GROUP BY analysis

---
+++ functions.sql
@@ -0,0 +1,24 @@
+SELECT CONCAT('Hello', ' ', 'World') AS greeting;
+-- Output: Hello World
+
+
+
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM employees;
+
+
+SELECT NOW();
+-- Output: 2025-05-03 14:20:45 (example)
+
+
+
+SELECT LENGTH('neo');
+
+
+SELECT ROUND(12.6789, 2);
+
+
+SELECT DATEDIFF('2025-06-01', '2025-05-01');
+
+
+
